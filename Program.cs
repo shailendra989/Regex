c2f586da83f81598efc33064b1f2606113484128
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-22 15:05:08 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace REG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mob = "^[0-9]{10}$";
+            string pinc = "^[0-9]{6}$";
+            string alpha = "^[A-Za-z]+$";
+            //string numbrbetween = "[a-zA-Z]*[0-9][A-Za-z]+";
+            string email = "[A-Za-z]*[0-9]*[@][A-Za-z]+[.][a-zA-Z]+";
+            string password = "[A-Z]{1}[A-Za-z]*[@][0-9]";
+            Regex obj = new Regex(password);
+            string s = "Shailendra@123";
+            bool r = obj.IsMatch(s);
+            Console.WriteLine(r);
+
+
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ fod.sh
@@ -0,0 +1,7 @@
+echo "# Regex" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/shailendra989/Regex.git
+git push -u origin main
